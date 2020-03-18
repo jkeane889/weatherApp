15cfbef1aa78812674ccf228c0966a0206342d71
jkeane889 <jkeane889@gmail.com>
2020-03-17 19:34:50 -0500

    Finished webpack and babel configuration

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const SRC_DIR = path.join(__dirname, '/client');
+const DIST_DIR = path.join(__dirname, '/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.js`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  module: {
+    rules: [
+      { test: /\.(js|jsx)$/, 
+        include : SRC_DIR,
+        exclude: /node_modules/,
+        loader: "babel-loader" 
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader', 
+          {
+            loader: 'css-loader',
+            options: {
+             importLoaders: 1, 
+             modules: true,
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]',
+        },
+      },
+    ]
+  }
+};
